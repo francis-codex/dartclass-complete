4a92ddf802e7d660fc994cdeee1d0675eba09a04
francis-codex <francislogo2219@gmail.com>
2024-05-08 14:40:09 +0100

    dartclass pushed

---
+++ logic.dart
@@ -0,0 +1,28 @@
+void main() {
+	// Logic - If, If/Else, If Else If
+
+ var num = 3;
+  if ( num == 5){
+    print("this is number is 5!");
+  }
+  else if ( num == 3){
+   print("the number is 3 now");
+  }
+  else{
+    print("the number is not 5 and is not 3 at all");
+  }
+
+	// var num = 51;
+	// if (num == 5){
+	// 	print("The Number Is 5!");
+	// } 
+	// else if (num == 3){
+	// 	print("The Number is 3!");
+
+	// } else {
+	// 	print("The Number is NOT 3 or 5, it is $num");
+
+	// }
+
+
+}
