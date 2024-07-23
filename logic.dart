19508dfe4bdc0ce7879ac81519943bae26f3af33
francis-codex <francislogo2219@gmail.com>
2024-07-23 22:13:34 +0100

    final

--- logic.dart
+++ logic.dart
@@ -1,16 +1,16 @@
 void main() {
 	// Logic - If, If/Else, If Else If
 
- var num = 3;
-  if ( num == 5){
-    print("this is number is 5!");
-  }
-  else if ( num == 3){
-   print("the number is 3 now");
-  }
-  else{
-    print("the number is not 5 and is not 3 at all");
-  }
+//  var num = 3;
+//   if ( num == 5){
+//     print("this is number is 5!");
+//   }
+//   else if ( num == 3){
+//    print("the number is 3 now");
+//   }
+//   else{
+//     print("the number is not 5 and is not 3 at all");
+//   }
 
 	// var num = 51;
 	// if (num == 5){
